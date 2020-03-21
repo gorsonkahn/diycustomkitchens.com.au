3fe7d734c85871e39f3d69a7cd608e651da19558
Unknown <gorson01@hotmail.com>
2020-03-21 21:09:14 +1100

    Updating from prod

--- sites/all/themes/cabtek/js/scripts.js
+++ sites/all/themes/cabtek/js/scripts.js
@@ -1,4 +1,3 @@
-
 (function ($) {  
 
   Drupal.behaviors.cabtek = {
@@ -39,43 +38,33 @@
 			// SHOW PRICE FIELD CHECKS
 			// ***
 			
-			// function showPrice(){
-			// 	$('.field-commerce-price').show();
-			// }
-			// //FOR LENGTHS (KICKBOARDS) CHECK FOR A HEIGHT VALUE
-			// if($('body').hasClass('node-type-length')) {
-			// 	if ($("#edit-line-item-fields-field-height-und-0-value").val() > 0){
-			// 		showPrice();
-			// 	}
-			// }
-			// if($('body').hasClass('node-type-product-display')) {
-			// 	showPrice();
-			// }			
-			// if ($("#edit-line-item-fields-field-width-und-0-value").val() > 0){
-			// 	showPrice();
-			// }
-			// if ($(".field-name-commerce-price").text() === "$0.00") {
-			// 	$('.field-commerce-price').hide();
-			// }
+			function showPrice(){
+				$('.field-commerce-price').show();
+			}
+			//FOR LENGTHS (KICKBOARDS) CHECK FOR A HEIGHT VALUE
+			if($('body').hasClass('node-type-length')) {
+				if ($("#edit-line-item-fields-field-height-und-0-value").val() > 0){
+					showPrice();
+				}
+			}
+			if($('body').hasClass('node-type-product-display')) {
+				showPrice();
+			}			
+			if ($("#edit-line-item-fields-field-width-und-0-value").val() > 0){
+				showPrice();
+			}
+			if ($(".field-name-commerce-price").text() === "$0.00") {
+				$('.field-commerce-price').hide();
+			}
+			
 			
 			//ADD PRODUCT CAT VALUE TO LINE ITEM FOR USE BY ORDER EXPORT
 			var product_cat = $(".field-name-field-product-category").text();
 			$("#edit-line-item-fields-field-ez-category-text-und-0-value").val(product_cat);
-
 			
 			//ADD HINGE VALUE TO LINE ITEM FOR USE BY ORDER EXPORT
 			var hinge_cat = $(".field-name-field-ez-hinge-category").text();
 			$("#edit-line-item-fields-field-ez-hinge-text-und-0-value").val(hinge_cat);
-			
-			
-			//GET CUSTOM W,H,D DEFAULT VALUES PER PRODUCT DISPLAY AND WRITE TO LINE ITEM FIELD
-			// if ($(".field-name-field-defaults-set").text() == 0){
-				// var default_width = $(".field-name-field-width-default").text();
-				// $("#edit-line-item-fields-field-width-und-0-value").val(default_width);
-				// $(".field-name-field-defaults-set").val("1");
-			// }
-			
-			
 			
 			//SHOW RELEVANT SHIPPING SERVICES ON CHECKOUT PROCESS
 			var hw_status = $("#hard_status").text();
@@ -92,9 +81,6 @@
 				$( "#edit-commerce-shipping-shipping-service .form-item:nth-child(3)" ).hide();
 			}
 			
-			//KEEP CHECKOUT BUTTONS ENABLED
-			//alert('aa');
-			//$(':input[type="submit"]').prop('disabled', false);
 			
 		// end our js code
 		}
